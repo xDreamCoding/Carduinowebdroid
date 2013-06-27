ca0d90f5620a2c67da1b0ee34e7c3cc985c49be5
Christoph Braun <git@ragetroops.eu>
2013-06-27 19:14:10 +0200

    Fixes #28

--- carduinowebdroid/WebContent/WEB-INF/queue.jsp
+++ carduinowebdroid/WebContent/WEB-INF/queue.jsp
@@ -2,9 +2,10 @@
 <%@ taglib uri="/WEB-INF/lib/tags/customTag.tld" prefix="ct" %>
 
 <c:set var="qlist">
-	<ct:getQ />
+	<ct:getQ par="0" />
 </c:set>
 
+<h1>Current driver: <ct:getQ par="1" /></h1>
 <c:forEach items="${qlist}" var="s"> 
 	<c:out value="${s}" /><br>
 </c:forEach>
--- carduinowebdroid/src/de/carduinodroid/web/QueueTag.java
+++ carduinowebdroid/src/de/carduinodroid/web/QueueTag.java
@@ -5,24 +5,46 @@ import java.io.IOException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
-import de.carduinodroid.shared.*;
+
+import de.carduinodroid.shared.activeSession;
+import de.carduinodroid.shared.waitingqueue;
 
 public class QueueTag extends TagSupport {
 	
 	private static final long serialVersionUID = 1L;
+	private int parameter;
+	
+	public void setPar(int i) {
+		parameter = i ;
+	}
+	
 	
 	public int doStartTag() throws JspException{
 		
 		JspWriter out = pageContext.getOut();
-		String[] List = new  String [waitingqueue.getLength()];
-	    List = waitingqueue.getNickname();
+		String[] ListQ = new  String [waitingqueue.getLength()];
+		String[] ListSession = new  String [activeSession.getLength()];
+	    ListQ = waitingqueue.getNickname();
 		
-	    try {
-	    	for(String s : List)	
-				out.print(s+",");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    switch(parameter){
+	    case 0:
+	    	try {
+		    	for(String s : ListQ)	
+					out.print(s+",");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	break;
+	    case 1:
+	    	try {
+					out.print((String)activeSession.getDriver().getAttribute("nickName"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	
+	    }
+	    
+	    
 		
 		return EVAL_PAGE;
 	}
